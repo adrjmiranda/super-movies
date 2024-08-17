4d831c73244e92a45c2f4165f56c0422e1bb7d43
Adriano Miranda <adrjmiranda@gmail.com>
2024-08-17 16:53:44 -0300

    chore: adds navbar view for smaller screens

---
+++ app/src/js/site/navbar_toggle_menu.ts
@@ -0,0 +1,8 @@
+const navbarToggleMenu = document.querySelector(
+	'.navbar_toggle_menu'
+) as HTMLButtonElement;
+const navbarMenu = document.querySelector('.navbar_menu') as HTMLDivElement;
+
+navbarToggleMenu.addEventListener('click', () => {
+	navbarMenu.classList.toggle('show');
+});
