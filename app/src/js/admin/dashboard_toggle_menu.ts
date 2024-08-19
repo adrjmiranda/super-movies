c79b990859c1f5d87f73a2cd9e97fc241729ffd2
Adriano Miranda <adrjmiranda@gmail.com>
2024-08-19 01:04:10 -0300

    chore: Responsive version created for screens other than the admin dashboard

--- app/src/js/admin/dashboard_toggle_menu.ts
+++ app/src/js/admin/dashboard_toggle_menu.ts
@@ -5,16 +5,45 @@ const dashboardAside = document.querySelector(
 	'.dashboard_aside'
 ) as HTMLDivElement;
 
+const dashboardCloseMenuBtn = document.querySelector(
+	'.dashboard_close_menu'
+) as HTMLButtonElement;
+
 let state = false;
 
 dashboardToggleMenuBtn.addEventListener('click', () => {
+	let screenWidth = window.innerWidth;
+
+	if (dashboardAside) {
+		if (state) {
+			if (screenWidth > 630) {
+				dashboardAside.style.transform = 'translate(-100%, 0)';
+				dashboardAside.style.position = 'absolute';
+			}
+		} else {
+			if (screenWidth > 630) {
+				dashboardAside.style.transform = 'translate(0, 0)';
+				dashboardAside.style.position = 'static';
+			} else {
+				dashboardAside.style.transform = 'translate(0, 0)';
+				dashboardAside.style.position = 'absolute';
+				dashboardAside.style.zIndex = '1';
+				dashboardAside.style.left = '0';
+				dashboardAside.style.top = '0';
+				dashboardAside.style.width = '100vw';
+			}
+		}
+
+		state = !state;
+	}
+});
+
+dashboardCloseMenuBtn.addEventListener('click', () => {
 	if (dashboardAside) {
 		if (state) {
 			dashboardAside.style.transform = 'translate(-100%, 0)';
-			dashboardAside.style.position = 'absolute';
-		} else {
-			dashboardAside.style.transform = 'translate(0, 0)';
 			dashboardAside.style.position = 'static';
+			dashboardAside.style.width = 'unset';
 		}
 
 		state = !state;
