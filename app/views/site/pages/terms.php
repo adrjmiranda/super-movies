6824e32968138b890d82a0a98163f1c48da24ce8
Adriano Miranda <adrjmiranda@gmail.com>
2024-08-21 12:33:11 -0300

    chore: created terms page.

--- app/views/site/pages/terms.php
+++ app/views/site/pages/terms.php
@@ -8,4 +8,109 @@ $this->extends('layouts.master', [
 ]);
 ?>
 
-<h1>Terms</h1>
+<div class="terms_page">
+  <div class="container">
+    <h1>SuperMovies - Terms of Service</h1>
+
+    <div class="terms_content_block">
+      <h2>Introduction</h2>
+      <p>Welcome to SuperMovies! These Terms of Service ("Terms") govern your access to and use of the SuperMovies
+        platform,
+        including any content, functionality, and services offered on or through SuperMovies (the "Service"). By
+        accessing
+        or using the Service, you agree to comply with and be bound by these Terms. If you do not agree to these Terms,
+        you
+        must not access or use the Service.</p>
+    </div>
+
+
+    <div class="terms_content_block">
+      <h2>Eligibility</h2>
+      <p>To use the Service, you must be at least 18 years old, or if you are under 18, you must have the permission and
+        supervision of a parent or legal guardian who agrees to these Terms. By using the Service, you represent and
+        warrant
+        that you meet these eligibility requirements.</p>
+    </div>
+
+    <div class="terms_content_block">
+      <h2>Account Registration</h2>
+      <p>To access certain features of the Service, you may need to create an account. You agree to provide accurate,
+        current, and complete information during the registration process and to update such information to keep it
+        accurate, current, and complete. You are responsible for safeguarding your account credentials and for all
+        activity
+        that occurs under your account. You agree to notify us immediately of any unauthorized use of your account.</p>
+    </div>
+
+    <div class="terms_content_block">
+      <h2>Subscription and Billing</h2>
+      <p>SuperMovies offers various subscription plans. By subscribing to the Service, you agree to pay all fees and
+        charges
+        associated with your chosen subscription plan. Subscription fees are billed in advance on a recurring basis
+        according to the terms of the plan you select. You may cancel your subscription at any time through your account
+        settings. If you cancel, your access to the Service will continue until the end of your current billing cycle,
+        after
+        which your access will be terminated.</p>
+    </div>
+
+    <div class="terms_content_block">
+      <h2>Content and Licensing</h2>
+      <p>All content available on SuperMovies, including but not limited to films, videos, graphics, logos, and text, is
+        owned by or licensed to SuperMovies and is protected by intellectual property laws. You are granted a limited,
+        non-exclusive, non-transferable license to access and view the content for personal, non-commercial use only.
+        You
+        may not reproduce, distribute, modify, create derivative works of, publicly display, or exploit any content
+        without
+        the prior written consent of SuperMovies.</p>
+    </div>
+
+    <div class="terms_content_block">
+      <h2>Prohibited Conduct</h2>
+      <h3>You agree not to engage in any of the following prohibited activities:</h3>
+      <ul>
+        <li>Using the Service for any illegal purpose or in violation of any applicable laws.</li>
+        <li>Sharing your account credentials with others or allowing others to access the Service using your account.
+        </li>
+        <li>Attempting to hack, disrupt, or interfere with the security or functionality of the Service.</li>
+        <li>Uploading or transmitting any viruses, malware, or harmful content.</li>
+        <li>Engaging in any activity that may damage or negatively impact the reputation of SuperMovies.</li>
+      </ul>
+    </div>
+
+    <div class="terms_content_block">
+      <h2>Termination</h2>
+      <p>SuperMovies reserves the right to terminate or suspend your account and access to the Service at any time, with
+        or
+        without cause, and with or without notice. Upon termination, all rights and licenses granted to you under these
+        Terms will immediately cease.</p>
+    </div>
+
+    <div class="terms_content_block">
+      <h2>Limitation of Liability</h2>
+      <p>To the fullest extent permitted by law, SuperMovies shall not be liable for any indirect, incidental, special,
+        consequential, or punitive damages, including but not limited to loss of profits, data, or use, arising out of
+        or
+        in
+        connection with your use of or inability to use the Service.</p>
+    </div>
+
+    <div class="terms_content_block">
+      <h2>Governing Law</h2>
+      <p>These Terms and any disputes related to them or the Service will be governed by and construed in accordance
+        with
+        the
+        laws of the jurisdiction in which SuperMovies operates, without regard to its conflict of law provisions.</p>
+    </div>
+
+    <div class="terms_content_block">
+      <h2>Changes to the Terms</h2>
+      <p>SuperMovies reserves the right to modify or update these Terms at any time. Any changes will be effective
+        immediately upon posting the revised Terms on the Service. Your continued use of the Service after such changes
+        constitutes your acceptance of the new Terms.</p>
+    </div>
+
+    <div class="terms_content_block">
+      <h2>Contact Us</h2>
+      <p>If you have any questions or concerns about these Terms, please contact us at support@supermovies.com.</p>
+    </div>
+  </div>
+</div>
